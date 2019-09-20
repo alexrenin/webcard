ca518f80826d85da16b2a710d687b3be4c853d49
Alex <sanya_renin@mail.ru>
2019-09-20 17:06:33 +0500

    atomic finish + new template with i18n

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/index.css';
-import'./i18n';
-import i18n from "./i18n";
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './css/index.css'
+import'./i18n'
+import i18n from "./i18n"
+import App from './App'
 import { HashRouter } from 'react-router-dom'
 import storeFactory from './store/index'
 // import registerServiceWorker from './registerServiceWorker';
@@ -13,14 +13,18 @@ const store = storeFactory()
 window.React = React
 window.store = store
 
-//TODO лажа с i18n: непродуманая инициализация и взаимодествие с store
-i18n.changeLanguage(store.getState().language.listItems[store.getState().language.currentItem].text.toLowerCase())
+//initialize i18n
+i18n.changeLanguage(
+	store.getState().language
+		.listItems[store.getState().language.currentItem]
+			.text.toLowerCase()
+)
 
 ReactDOM.render(
 	<HashRouter>
 		<App store={store} />
 	</HashRouter>,
     document.getElementById('root')
-);
+)
 
 // registerServiceWorker();

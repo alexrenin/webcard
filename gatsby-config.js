c1bc64ade06e868cb6ed10db9af355215f854714
Alex <sanya_renin@mail.ru>
2020-02-24 10:29:26 +0300

    added new file for gatsby.js

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: 'AR develop',
+    author: 'Alex Renin',
+  },
+  plugins: [ ]
+}
---
+++ gatsby-node.js
@@ -0,0 +1,6 @@
+const path = require('path')
+
+module.exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  
+}

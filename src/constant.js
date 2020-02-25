a28332bb0be90487ef47e838192b7ca42965f707
Alex <sanya_renin@mail.ru>
2020-02-25 13:01:11 +0300

    constant.js fix part 2

---
+++ src/constant.js
@@ -0,0 +1,28 @@
+const constants = {
+    VER: '3.0.0',
+	home: 'home',
+	summary: 'summary',
+	portfolio: 'portfolio',
+	contacts: 'contacts',
+	SITE_URL: "http://localhost:3000",
+	DEVELOP_FLAG: false,
+	MIP_DESC_ID: "mipDescID",
+	JRSS_DESC_ID: "jrssDescID",
+	PP_DESC_ID: "ppDescID",
+    locales: [
+        {
+            langID: 'ru',
+            path: 'ru',
+            name: 'Русский',
+        },
+        {
+            langID: 'en',
+            path: 'en',
+            name: 'English',
+            isDefault: true,
+        }
+    ]
+}
+
+export default constants
+

57bf8a6fed177dd921a180c818113ffeda7e3895
Alex <sanya_renin@mail.ru>
2020-02-27 15:55:49 +0300

    fix error with icon attribute

--- src/components/atoms/icons/icons.js
+++ src/components/atoms/icons/icons.js
@@ -5,7 +5,6 @@ export function TelegramIcon() {
         <svg
             xmlns="http://www.w3.org/2000/svg"
             id="Bold"
-            enable-background="new 0 0 24 24"
             height="512"
             viewBox="0 0 24 24"
             width="512"

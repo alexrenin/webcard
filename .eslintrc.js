ffe4d73a75a53551785faa2f91f05500f1044956
alexrenin <alexrenin.info@gmail.com>
2020-12-03 21:02:17 +0300

    eslint airbnb settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,27 @@
 module.exports = {
-    globals: {
-        __PATH_PREFIX__: true,
+  globals: {
+    __PATH_PREFIX__: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+  ],
+  plugins: [
+    'jsx-a11y',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/require-default-props': [1, { forbidDefaultForRequired: true, ignoreFunctionalComponents: true }],
+    indent: ['error', 2],
+    'react/jsx-indent': ['error', 2],
+    'react/jsx-indent-props': ['error', 2],
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+      },
     },
-    'extends': [
-        'react-app',
-    ],
-    'plugins': [
-        'jsx-a11y'
-    ],
-    'rules': {
-        'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
-        'react/require-default-props': [1, { forbidDefaultForRequired: true, ignoreFunctionalComponents: true }],
-        "indent": ["error", 2],
-        "react/jsx-indent": ["error", 2],
-        "react/jsx-indent-props": ["error", 2],
-        "no-console": ["error", { allow: ["warn", "error"] }]
-    },
-    'settings': {
-        'import/resolver': {
-            'node': {
-                'paths': ['src']
-            }
-        }
-    }
+  },
 };
-
--- gatsby-node.js
+++ gatsby-node.js
@@ -2,37 +2,49 @@ const path = require('path')
 
 const C = require('./constants_node')
 
+module.exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        components: path.resolve(__dirname, 'src/components'),
+        pages: path.resolve(__dirname, 'src/pages'),
+        config: path.resolve(__dirname, 'src/config'),
+      },
+    },
+  })
+}
+
 module.exports.onCreatePage = ({ page, actions }) => {
-    const { createPage, deletePage } = actions
+  const { createPage, deletePage } = actions
 
-    // First delete the incoming page that was automatically created by Gatsby
-    // So everything in src/pages/
-    deletePage(page)
+  // First delete the incoming page that was automatically created by Gatsby
+  // So everything in src/pages/
+  deletePage(page)
 
-    const { locales = [] } = C
+  const { locales = [] } = C
 
-    locales.forEach( ({ path, isDefault, contentfulID }) => {
-        const localizedPath = isDefault
-            ? page.path
-            : `/${path}${page.path}`
-        
-        return createPage({
-            ...page,
-            path: localizedPath,
-            context: {
-                ...page.context,
-                locale: contentfulID,
-                // dateFormat: dateFormat,
-            },
-        })
+  locales.forEach( ({ path, isDefault, contentfulID }) => {
+    const localizedPath = isDefault
+      ? page.path
+      : `/${path}${page.path}`
+
+    return createPage({
+      ...page,
+      path: localizedPath,
+      context: {
+        ...page.context,
+        locale: contentfulID,
+        // dateFormat: dateFormat,
+      },
     })
+  })
 }
 
 module.exports.createPages = async ({ graphql, actions }) => {
-    const { createPage } = actions
-    const portfolioItemTemplate = path.resolve('./src/components/templates/portfolioItem/portfolioItem.js')
+  const { createPage } = actions
+  const portfolioItemTemplate = path.resolve('./src/components/templates/portfolioItem/portfolioItem.js')
 
-    const res = await graphql(`
+  const res = await graphql(`
         query  {
           allContentfulPortfolioItem {
             edges {
@@ -44,22 +56,22 @@ module.exports.createPages = async ({ graphql, actions }) => {
         }
     `)
 
-    const { locales = [] } = C
-    locales.forEach( ({ path, isDefault, contentfulID }) => {
-        const localizedPath = isDefault
-            ? ""
-            : `/${path}`
+  const { locales = [] } = C
+  locales.forEach( ({ path, isDefault, contentfulID }) => {
+    const localizedPath = isDefault
+      ? ""
+      : `/${path}`
 
-        res.data.allContentfulPortfolioItem.edges.forEach( edge => {
-            createPage({
-                component: portfolioItemTemplate,
-                path: `${localizedPath }/portfolio/${edge.node.slug}`,
-                context: {
-                    slug: edge.node.slug,
-                    locale: contentfulID,
-                }
-            })
-        })
+    res.data.allContentfulPortfolioItem.edges.forEach( edge => {
+      createPage({
+        component: portfolioItemTemplate,
+        path: `${localizedPath }/portfolio/${edge.node.slug}`,
+        context: {
+          slug: edge.node.slug,
+          locale: contentfulID,
+        }
+      })
     })
+  })
 
 }

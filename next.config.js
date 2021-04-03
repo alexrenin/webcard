51cbdf3850ccfeeb58ffeb40ea6daa4f68f7fb2d
alexrenin <alexrenin.info@gmail.com>
2021-04-04 01:46:44 +0300

    deploy test

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 const path = require('path');
 
 module.exports = {
-  i18n: {
-    locales: ['ru-RU'],
-    defaultLocale: 'ru-RU',
-    domains: [
-      {
-        domain: 'alexrenin.info',
-        defaultLocale: 'ru-RU',
-      },
-    ],
-  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'src')],
   },

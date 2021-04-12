12779a0bad76f5aebb8629b7b95a7ad481ca74b5
alexrenin <alexrenin.info@gmail.com>
2021-04-12 11:57:53 +0300

    migration atoms to ts

--- src/components/atoms/CustomButton/CustomButton.tsx
+++ src/components/atoms/CustomButton/CustomButton.tsx
@@ -1,79 +1,40 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import makeStyles from '@material-ui/core/styles/makeStyles';
 import Button from '@material-ui/core/Button';
 
-import buttonStyle from './styles';
+import { useStyles, ICustomButtonStyleProps, PropsClasses } from './styles';
 
-const makeComponentStyles = makeStyles(() => ({
-  ...buttonStyle,
-}));
+export interface ICustomButtonProps {
+  color: 'primary' | 'transparent',
+  children: React.ReactNode | React.ReactNodeArray,
+  className: boolean,
+}
 
-const CustomButton = React.forwardRef((props, ref) => {
-  const {
-    color,
-    round,
-    children,
-    fullWidth,
-    disabled,
-    simple,
-    size,
-    block,
-    link,
-    justIcon,
-    className,
-    ...rest
-  } = props;
+export type Ref = HTMLButtonElement;
 
-  const classes = makeComponentStyles();
+const CustomButton: React.FC = React.forwardRef<Ref, ICustomButtonProps>(({
+  color,
+  className,
+  children,
+  ...rest
+}: ICustomButtonProps, ref) => {
+  const classes: PropsClasses = useStyles({} as ICustomButtonStyleProps);
 
   const btnClasses = [
     classes.button,
-    classes[size],
     classes[color],
-    round ? classes.round : '',
-    fullWidth ? classes.fullWidth : '',
-    disabled ? classes.disabled : '',
-    simple ? classes.simple : '',
-    block ? classes.block : '',
-    link ? classes.link : '',
-    justIcon ? classes.justIcon : '',
     className || '',
   ].join(' ');
 
   return (
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    <Button {...rest} ref={ref} className={btnClasses}>
+    <Button
+      ref={ref}
+      className={btnClasses}
+      /* eslint-disable-next-line react/jsx-props-no-spreading */
+      {...rest}
+    >
       {children}
     </Button>
   );
 });
-
-CustomButton.propTypes = {
-  color: PropTypes.oneOf([
-    'primary',
-    'info',
-    'success',
-    'warning',
-    'danger',
-    'rose',
-    'white',
-    'facebook',
-    'twitter',
-    'google',
-    'github',
-    'transparent',
-  ]),
-  size: PropTypes.oneOf(['sm', 'lg']),
-  simple: PropTypes.bool,
-  round: PropTypes.bool,
-  fullWidth: PropTypes.bool,
-  disabled: PropTypes.bool,
-  block: PropTypes.bool,
-  link: PropTypes.bool,
-  justIcon: PropTypes.bool,
-  children: PropTypes.node,
-  className: PropTypes.string,
-};
 
 export default CustomButton;

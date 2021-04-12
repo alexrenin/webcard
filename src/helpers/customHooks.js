c9285e2e4f59e7d9cb4fd15d13d22a8943c7ba82
alexrenin <alexrenin.info@gmail.com>
2021-04-12 12:42:15 +0300

    fix error

--- src/helpers/customHooks.js
+++ src/helpers/customHooks.js
@@ -2,8 +2,8 @@ import { useState, useEffect } from 'react';
 
 export function useWindowSize() {
   const [windowSize, setWindowSize] = useState({
-    width: undefined,
-    height: undefined,
+    width: 320,
+    height: 720,
   });
 
   useEffect(

359da42f44c4e7360ded066a78126587d2258c22
alexrenin <alexrenin.info@gmail.com>
2021-04-14 11:41:30 +0300

    NextBackIconBtn to ts

--- src/components/molecules/NextBackIconBtn/NextBackIconBtn.tsx
+++ src/components/molecules/NextBackIconBtn/NextBackIconBtn.tsx
@@ -1,23 +1,10 @@
 import React from 'react';
 
-import { withStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 
-const CustomNextBackIconBtn = withStyles((theme) => ({
-  root: {
-    height: 32,
-    width: 32,
-    padding: 0,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    '& :hover svg circle': {
-      //fill: theme.palette.greyC.lightGrey,
-    },
-  },
-}))(IconButton);
+import styles from '.'
 
 function NextBackIconBtn({
   className = '',
@@ -25,7 +12,7 @@ function NextBackIconBtn({
   onClick = () => {},
 }) {
   return (
-    <CustomNextBackIconBtn
+    <IconButton
       className={className}
       onClick={onClick}
       aria-label={isBack ? 'back' : 'forward'}
@@ -36,7 +23,7 @@ function NextBackIconBtn({
       {!isBack && (
         <ArrowForwardIosIcon />
       )}
-    </CustomNextBackIconBtn>
+    </IconButton>
   );
 }
 
